75987a7f6c294681ec32c001e0173fc18ab1f1c2
sara sobhy <sarasobhy835@gmail.com>
2024-01-09 17:55:27 +0200

    link screen

--- Tic Tac Toe server/src/Mainpkg/Main.java
+++ Tic Tac Toe server/src/Mainpkg/Main.java
@@ -11,23 +11,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Dell
- */
+
 public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Screens/Home_Screen.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
--- Tic Tac Toe server/src/Screens/Chart_ScreenController.java
+++ Tic Tac Toe server/src/Screens/Chart_ScreenController.java
@@ -1,27 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Screens;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Dell
- */
-public class Chart_ScreenController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+public class Chart_ScreenController{
+
+   
+    private Stage stage;
+     private Scene scene;
+     private Parent root;
+    
+    public void switchToHome(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/Screens/Home_Screen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
