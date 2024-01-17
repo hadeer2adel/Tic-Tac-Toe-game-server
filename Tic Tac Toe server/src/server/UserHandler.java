6779b0f26bade25f5b895da8a9ab4407d4713b8a
Aya Mashaly <ayamashaly@gmail.com>
2024-01-17 14:31:16 +0200

    implement getAvaliable players connection

--- Tic Tac Toe server/src/server/UserHandler.java
+++ Tic Tac Toe server/src/server/UserHandler.java
@@ -18,41 +18,44 @@ import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
-public class UserHandler extends Thread
-{
+public class UserHandler extends Thread {
+
     private static Vector<UserHandler> UserHandlers = new Vector<>();
-    
+
     private int id;
     private boolean online;
     private Socket clientSocket;
     private DataInputStream ear;
     private DataOutputStream mouth;
-        
-   public UserHandler(Socket cs) {
+
+    public UserHandler(Socket cs) {
         System.out.println("server.UserHandler.<init>()");
         clientSocket = cs;
         start();
     }
-    
+
     @Override
-    public void run(){
+    public void run() {
         System.out.println("server.UserHandler.run()");
-        while(true){
+        while (true) {
             try {
                 ear = new DataInputStream(clientSocket.getInputStream());
                 mouth = new DataOutputStream(clientSocket.getOutputStream());
                 String serverResponse = ear.readUTF();
                 JsonReader jsonReader = Json.createReader(new StringReader(serverResponse));
                 JsonObject requestJson = jsonReader.readObject();
-                
-                if(requestJson.containsKey("response"))
+
+                if (requestJson.containsKey("response")) {
                     handleResponse(requestJson);
-                else if(requestJson.containsKey("request"))
+                } else if (requestJson.containsKey("request")) {
                     handleRequest(requestJson);
-                
+                }
+
             } catch (IOException ex) {
                 try {
                     online = false;
@@ -67,7 +70,7 @@ public class UserHandler extends Thread
             }
         }
     }
-    
+
     private void handleResponse(JsonObject responseJson) {
         System.out.println("server.UserHandler.handleResponse()");
         try {
@@ -84,7 +87,7 @@ public class UserHandler extends Thread
             Logger.getLogger(UserHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-  
+
     private void handleRequest(JsonObject requestJson) {
         System.out.println("server.UserHandler.handleRequest()");
         try {
@@ -95,6 +98,9 @@ public class UserHandler extends Thread
                     break;
                 case "signup":
                     signUp(requestJson);
+                    break;
+                case "availablePlayers":
+                    getAvailablePlayers(requestJson);
                     break;
                 case "invite":
                     sendInvitation(requestJson);
@@ -110,45 +116,43 @@ public class UserHandler extends Thread
         }
     }
 
-
-    private void login(JsonObject requestJson) throws SQLException, IOException{
+    private void login(JsonObject requestJson) throws SQLException, IOException {
         System.out.println("server.UserHandler.login()");
         String email = requestJson.getString("email");
         String password = requestJson.getString("password");
-        
+
         UserData player = DataAccessObject.getUser(email, password);
 
         if (player != null) {
             player.setIs_available(true);
             player.setIs_onGame(false);
             DataAccessObject.updateStatus(player);
-            
+
             online = true;
             id = player.getId();
             UserHandlers.add(this);
-            
+
             JsonObject responseJson = Json.createObjectBuilder()
-                    .add("response","login")
-                    .add("status","success")
-                    .add("id",player.getId())
-                    .add("name",player.getName())
-                    .add("email",player.getEmail())
-                    .add("password",player.getPassword())
-                    .add("score",player.getScore())
-                    .add("is_available",player.getIs_available())
-                    .add("is_onGame",player.getIs_onGame())
+                    .add("response", "login")
+                    .add("status", "success")
+                    .add("id", player.getId())
+                    .add("name", player.getName())
+                    .add("email", player.getEmail())
+                    .add("password", player.getPassword())
+                    .add("score", player.getScore())
+                    .add("is_available", player.getIs_available())
+                    .add("is_onGame", player.getIs_onGame())
                     .build();
             mouth.writeUTF(responseJson.toString());
-        } 
-        else {
+        } else {
             JsonObject responseJson = Json.createObjectBuilder()
-                    .add("response","login")
-                    .add("status","fail")
+                    .add("response", "login")
+                    .add("status", "fail")
                     .build();
             mouth.writeUTF(responseJson.toString());
         }
     }
-    
+
     private void signUp(JsonObject requestJson) throws SQLException, IOException {
         System.out.println("server.UserHandler.signUp()");
         String email = requestJson.getString("email");
@@ -156,26 +160,56 @@ public class UserHandler extends Thread
         String name = requestJson.getString("name");
         UserData user = new UserData(MIN_PRIORITY, name, email, password, MIN_PRIORITY, true, true);
         int added = DataAccessObject.addUser(user);
-        
-        if(added > 0 )
-        {
+
+        if (added > 0) {
             JsonObject responseJson = Json.createObjectBuilder()
-                    .add("response","signup")
-                    .add("status","success")
+                    .add("response", "signup")
+                    .add("status", "success")
+                    .build();
+            mouth.writeUTF(responseJson.toString());
+        } else {
+            JsonObject responseJson = Json.createObjectBuilder()
+                    .add("response", "signup")
+                    .add("status", "fail")
                     .build();
             mouth.writeUTF(responseJson.toString());
         }
-        else {
-            JsonObject responseJson = Json.createObjectBuilder()
-                    .add("response","signup")
-                    .add("status","fail")
-                    .build();
-            mouth.writeUTF(responseJson.toString());
-        }
-        
+
     }
 
-    private void sendInvitation(JsonObject requestJson) throws IOException{
+    private void getAvailablePlayers(JsonObject requestJson) throws SQLException, IOException {
+        int id = requestJson.getInt("id");
+        JsonArrayBuilder usersBuilder = Json.createArrayBuilder();
+
+        ArrayList<UserData> onlinePlayers = DataAccessObject.getAvailableUser();
+        for (UserData player : onlinePlayers) {
+            if(id != player.getId()){
+                JsonObject user = Json.createObjectBuilder()
+                    .add("id", player.getId())
+                    .add("name", player.getName())
+                    .add("score", player.getScore())
+                    .build();
+            usersBuilder.add(user);
+            }
+        }
+        if(!onlinePlayers.isEmpty()){
+            JsonArray users = usersBuilder.build();
+            JsonObject responseJson = Json.createObjectBuilder()
+                    .add("response", "availablePlayers")
+                    .add("status", "success")
+                    .add("players", users)
+                    .build();
+            mouth.writeUTF(responseJson.toString());
+        } else {
+            JsonObject responseJson = Json.createObjectBuilder()
+                    .add("response", "login")
+                    .add("status", "fail")
+                    .build();
+            mouth.writeUTF(responseJson.toString());
+        }
+    }
+
+    private void sendInvitation(JsonObject requestJson) throws IOException {
         System.out.println("server.UserHandler.sendInvitation()");
         int id2 = requestJson.getInt("id2");
         UserHandler player2 = getUser(id2);
@@ -183,20 +217,19 @@ public class UserHandler extends Thread
         mouth.flush();
     }
 
-    private void receiveInvitation(JsonObject responseJson) throws IOException{
+    private void receiveInvitation(JsonObject responseJson) throws IOException {
         System.out.println("server.UserHandler.receiveInvitation()");
         int id1 = responseJson.getInt("id1");
         UserHandler player1 = getUser(id1);
         player1.mouth.writeUTF(responseJson.toString());
         mouth.flush();
     }
-    
-    private UserHandler getUser(int id){
+
+    private UserHandler getUser(int id) {
         System.out.println("server.UserHandler.getUser()");
         UserHandler userHandler = null;
-        for(UserHandler user : UserHandlers){
-            if(user.online && user.id == id)
-            {
+        for (UserHandler user : UserHandlers) {
+            if (user.online && user.id == id) {
                 userHandler = user;
             }
         }
